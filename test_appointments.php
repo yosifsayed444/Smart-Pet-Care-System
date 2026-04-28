484d192055409f8abe016ed741a515bb9404f124
Yossef Sayed <yousifsayed338@gmail.com>
2026-04-29 00:59:14 +0300

    chore: remove obsolete test file and sample accounts data

--- test_appointments.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'App/Core/init.php';
-$appointment = new Appointment();
-$appointments = $appointment->fetchAll();
-var_dump($appointments);
-$totalAppointments = is_array($appointments) ? count($appointments) : 0;
-var_dump($totalAppointments);
